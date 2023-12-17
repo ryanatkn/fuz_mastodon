50c449780444d81430b76d52f89d78bbad2d17bb
Ryan Atkinson <mail@ryanatkn.com>
2023-12-17 09:10:35 -0700

    wip

---
+++ src/routes/mastodon_fake_cache_data.ts
@@ -0,0 +1,4 @@
+import type {Url} from '@grogarden/gro/paths.js';
+import type {Fetch_Value_Cache_Item} from '@grogarden/util/fetch.js';
+
+export const mastodon_fake_cache_data: Array<[Url, Fetch_Value_Cache_Item]> = [];
