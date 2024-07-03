b559d37a2c5d1c37ce44e63537d2dc9d719f0551
Ryan Atkinson <mail@ryanatkn.com>
2024-07-03 12:06:34 -0600

    update to latest `create-svelte`

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import type {UserConfig} from 'vite';
+import {defineConfig} from 'vite';
 import {sveltekit} from '@sveltejs/kit/vite';
 import {remove_prism_css} from '@ryanatkn/fuz_code/remove_prism_css_vite_plugin.js';
 
-const config: UserConfig = {
+export default defineConfig({
 	plugins: [sveltekit(), remove_prism_css()],
-};
-
-export default config;
+});
