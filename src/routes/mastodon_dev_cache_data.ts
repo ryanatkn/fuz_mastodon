aab5071e3a285e6cefb83fc92cf5c086abc836d0
Ryan Atkinson <mail@ryanatkn.com>
2025-08-03 22:54:27 -0400

    wip

--- src/routes/mastodon_dev_cache_data.ts
+++ src/routes/mastodon_dev_cache_data.ts
@@ -1,4 +1,4 @@
-import type {Url} from '@ryanatkn/gro/package_json.js';
+import type {Url} from '@ryanatkn/belt/url.js';
 import type {Fetch_Value_Cache_Item} from '@ryanatkn/belt/fetch.js';
 
 export const mastodon_dev_cache_data: Array<[Url, Fetch_Value_Cache_Item]> = [
