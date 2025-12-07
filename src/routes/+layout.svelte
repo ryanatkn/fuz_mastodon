dd4c21115a033b78f112a11222fe07c66f24bfe1
Ryan Atkinson <mail@ryanatkn.com>
2025-12-07 17:14:51 -0500

    upgrade dev deps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '@fuzdev/fuz_css/style.css';
 	import '@fuzdev/fuz_css/theme.css';
 	import '@fuzdev/fuz_code/theme.css';
-	import '$routes/moss.css';
+	import '$routes/fuz.css';
 	import '$routes/style.css';
 
 	import Themed from '@fuzdev/fuz_ui/Themed.svelte';
