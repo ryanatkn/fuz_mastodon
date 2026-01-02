dea18aaccae63a7c34e9f2e45367790121beb1d6
Ryan Atkinson <mail@ryanatkn.com>
2026-01-02 13:21:40 -0500

    upgrade fuz_ui

--- src/routes/library.gen.ts
+++ src/routes/library.gen.ts
@@ -1,4 +1,4 @@
-import type {Gen} from '@ryanatkn/gro';
 import {library_gen} from '@fuzdev/fuz_ui/library_gen.js';
+import {library_throw_on_duplicates} from '@fuzdev/fuz_ui/library_generate.js';
 
-export const gen: Gen = library_gen();
+export const gen = library_gen({on_duplicates: library_throw_on_duplicates});
